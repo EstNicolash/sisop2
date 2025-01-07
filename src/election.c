3b9d4d2a814f82935766d7e12a8aff7bbc5f1dc4
Nicolas A. N. S. <nicolas.andre263@protonmail.com>
2025-01-06 22:16:48 -0300

    ???????????

--- src/election.c
+++ src/election.c
@@ -134,32 +134,22 @@ void send_election_message(struct election_msg msg) {
   addr.sin_port = htons(ELECTION_PORT);
   inet_pton(AF_INET, server_ips[next_server], &addr.sin_addr);
 
-  fprintf(stderr, "send_election_message: to %s,%d\n", server_ips[next_server],
-          ELECTION_PORT);
   int sockfd;
-  int is_sended = 0;
-
-  while (is_sended == 0) {
-
-    fprintf(stderr, "\t send_election_message loop\n");
-
-    sleep(1);
+  while (1) {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    fprintf(stderr, "\t sock\n");
     if (sockfd <= 0) {
       perror("Socket creation failed");
       sleep(1);
       continue;
     }
 
-    fprintf(stderr, "\t send_election_message tryconnect\n");
-    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) > 0) {
-      is_sended = 1;
-    } else {
-      perror("\t Connection failed, retrying...");
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+      perror("Connection failed, retrying...");
       close(sockfd);
+      sleep(1);
+      continue;
     }
+    break;
   }
 
   fprintf(stderr, "send_election_message sending message\n");
